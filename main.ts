3f73f4005ebca80b627879d2bc3d26d7dd60b1ee
yisedviviana <71479113+yisedviviana@users.noreply.github.com>
2020-09-17 22:20:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,119 @@
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # # . . #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
